f67dd8e0f75a4e4aec5decf753c5d1705a5a3796
Loghan <loghan.r@gmail.com>
2020-07-11 17:16:21 +0200

    init API + first route - get all user

---
+++ index.js
@@ -0,0 +1,39 @@
+const User = require('./lib/user');
+const { SERVER_PORT } = require('./lib/const');
+const bodyParser = require('body-parser');
+const Express = require('express');
+const Cors = require('cors');
+const Path = require('path');
+const logger = require("morgan");
+const app = Express();
+
+//** PARAMETERS **//
+
+app.use(logger('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(Cors({origin: 'http://localhost:5000'}));
+//app.use(Cors({origin: 'http://109.31.193.30'}));
+// app.use(Cors({origin: 'http://ideal-lnt.fr'}));
+app.use(Express.static(Path.join(__dirname, 'public')));
+app.set('views', Path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//** DEFAULT PATH **//
+
+app.get('/', (req, res) => { res.send('Server is open!') } );
+
+//** USER PATH **//
+
+app.get('/listUsers', User.getAll);
+/*app.get('/listUsers/:id', User.getOne);
+app.post('/addUser', User.addOne);
+app.post('/updateUser/:id', User.updateOne);
+app.delete('/deleteUser/:id', User.deleteOne);
+app.post('/login', User.login);*/
+
+//** SERVER LISTEN **//
+
+app.listen(SERVER_PORT, () => {
+   console.debug(`Server listening on : ${SERVER_PORT}`);
+});
